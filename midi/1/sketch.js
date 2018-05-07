228f809423a63d053af6538013bf87209eac324a
Niels NTG <NTG999@gmail.com>
2018-05-07 21:36:32 +0200

    Expanded documentation

--- midi/1/sketch.js
+++ midi/1/sketch.js
@@ -1,96 +1,32 @@
-// Audio in + 4 channel MIDI visualiser
+// 1 channel MIDI pitch visualisation
 
-// TODO documentation
-// TODO fft visualisation
-// TODO tweakables UI
-
-var mic;
-
-var fft;
-
-var recordHighLevel = 0;
-
-var channels = [
-	{
-		text: 'BUMM',
-		active: false,
-		enableAddress: 144,
-		disableAddress: 128,
-		pitch: 0,
-		gate: 0,
-	},
-	{
-		text: 'TSCHACK',
-		active: false,
-		enableAddress: 145,
-		disableAddress: 129,
-		pitch: 0,
-		gate: 0
-	},
-	{
-		text: 'TZINKIZIKIZIK',
-		active: false,
-		enableAddress: 146,
-		disableAddress: 130,
-		pitch: 0,
-		gate: 0
-	},
-	{
-		text: 'LAAAALALALAAA',
-		active: false,
-		enableAddress: 147,
-		disableAddress: 131,
-		pitch: 0,
-		gate: 0
-	}
-];
+// Current pitch level received from MIDI input
+var currentPitch = 0;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	mic = new p5.AudioIn();
-	mic.start();
-
-	fft = new p5.FFT();
-	fft.setInput(mic);
 
 	colorMode(HSB, 360, 100, 100);
-	textFont('serif');
 	textAlign(LEFT, TOP);
 	textSize(100);
 	strokeWeight(4);
+	noFill();
 }
 
 function draw() {
-	background(0, 0, 10, 1);
+	background(0, 0, 10, 0.1);
 
-	var micLevel = mic.getLevel();
-	if (micLevel > recordHighLevel) {
-		recordHighLevel = micLevel;
-	}
-
-	var recordLevelRadius = lerp(4, width / 2, recordHighLevel);
-	noFill();
-	stroke(0, 100, 100);
-	ellipse(width / 2, height, recordLevelRadius, recordLevelRadius);
-
-	var currentLevelRadius = lerp(4, width / 2, micLevel);
-	noStroke();
-	fill(200);
-	ellipse(width / 2, height, currentLevelRadius, currentLevelRadius);
-
-	noStroke();
-	for (let i = 0; i < channels.length; i++) {
-		const channel = channels[i];
-		if (channel.active) {
-			text(channel.text, 32, 32 + (100 * i));
-		}
-	}
+	// Visualise current pitch with circle radius
+	var r = map(currentPitch, 0, 100, 0, width);
+	stroke(map(currentPitch, 0, 100, 0, 360), 100, 100);
+	ellipse(width / 2, height / 2, r, r);
 }
 
 function keyPressed() {
 	if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
 }
 
+// Listen for incoming MIDI messages
 p5.midi.onInput = function(e) {
 	var address = e.data[0]
 	var pitch = e.data[1];
@@ -98,17 +34,5 @@ p5.midi.onInput = function(e) {
 
 	console.log(e.data);
 
-	for (let i = 0; i < channels.length; i++) {
-		let channel = channels[i];
-		if (address === channel.enableAddress) {
-			channel.active = true;
-		} else if (address === channel.disableAddress) {
-			channel.active = false;
-		}
-
-		if (address === channel.enableAddress || address === channel.disableAddress) {
-			channel.pitch = pitch;
-			channel.gate = gate;
-		}
-	}
+	currentPitch = pitch;
 }
