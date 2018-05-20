294e89a5b1a8096618d53e4a290c0a365ce3b020
Niels NTG <NTG999@gmail.com>
2018-05-20 14:41:39 +0200

    Add basic speech recognition sketch

---
+++ sketches/sound/5-speech-recognition/sketch.js
@@ -0,0 +1,41 @@
+// Simple sketch demonstrating speech recognition and text to speech using the [p5.Speech](http://ability.nyu.edu/p5.js-speech/) library.
+
+var textElement;
+
+var speechRec;
+var speech;
+
+function setup() {
+	noCanvas();
+
+	// Setup speech synth
+	speech = new p5.Speech();
+
+	// Create text element on the page
+	textElement = createDiv('say something');
+
+	// Setup speech recognition
+	speechRec = new p5.SpeechRec();
+
+	// Show speech recognition results in textElement
+	speechRec.onResult = onSpeak;
+
+	// Whether the speech recognition engine will give results continuously (true) or just once (false = default)
+	speechRec.continuous = false;
+
+	// Whether the speech recognition engine will give faster, partial results (true) or wait for the speaker to pause (false = default)
+	speechRec.interimResults = false;
+
+	// Start listening for speech
+	speechRec.start();
+}
+
+function onSpeak() {
+	console.log(speechRec.resultString, '\n(' + speechRec.resultConfidence * 100 + '% confidence)');
+	textElement.html(speechRec.resultString);
+	sayText();
+}
+
+function sayText() {
+	speech.speak(textElement.html());
+}
